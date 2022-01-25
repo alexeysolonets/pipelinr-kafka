d7b298c227d72ada15e689921991d4c81ef55045
Alexey Solonets <alexey.solonets@gmail.com>
2022-01-25 23:16:52 +0300

    init hello world

---
+++ src/test/java/ru/alexeysolonets/PipelinRkafka/PipelinRKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexeysolonets.PipelinRkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinRKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
